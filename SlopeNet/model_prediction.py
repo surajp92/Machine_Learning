6fc47c6d92ad57bf7e0418b76b7807e53216ba8a
surajp92 <surajp92@vt.edu>
2019-03-25 19:39:39 -0500

    Leapfrog scheme, sequential scheme added for DNN

--- SlopeNet/model_prediction.py
+++ SlopeNet/model_prediction.py
@@ -26,6 +26,18 @@ def model_predict(testing_set, m, n, dt, legs, slopnet):
     elif (legs == 4) & (slopnet == "BDF"):
         ytest_ml = model_predict_bdf4(testing_set, m, n, dt)
         return ytest_ml
+    elif (legs == 1) & (slopnet == "LEAPFROG"):
+        ytest_ml = model_predict_lf1(testing_set, m, n, dt)
+        return ytest_ml
+    elif (legs == 2) & (slopnet == "LEAPFROG"):
+        ytest_ml = model_predict_lf2(testing_set, m, n, dt)
+        return ytest_ml
+    elif (legs == 4) & (slopnet == "LEAPFROG"):
+        ytest_ml = model_predict_lf4(testing_set, m, n, dt)
+        return ytest_ml
+    elif (legs == 1) & (slopnet == "SEQ"):
+        ytest_ml = model_predict_seq1(testing_set, m, n, dt)
+        return ytest_ml
     
 def model_predict_bdf2(testing_set, m, n, dt):
     print("bdf2")
@@ -107,3 +119,95 @@ def model_predict_bdf4(testing_set, m, n, dt):
         ytest = ee.reshape(1,4*(n-1)) 
     
     return ytest_ml
+
+def model_predict_lf1(testing_set, m, n, dt):
+    print("lf1")
+    custom_model = load_model('best_model.hd5',custom_objects={'coeff_determination': coeff_determination})
+    
+    # create input at t= 0 for the model testing
+    ytest = [testing_set[0]]
+    ytest = np.array(ytest)
+    
+    ytest_ml = [testing_set[0]]
+    ytest_ml = np.array(ytest_ml)
+    ytest_ml = np.vstack((ytest_ml, testing_set[1]))
+    
+    for i in range(2,testing_set.shape[0]-1):
+        slope_ml = custom_model.predict(ytest) # slope from LSTM/ ML model
+        a = ytest_ml[i-2] + 2.0*dt*slope_ml[0] # y1 at next time step
+        ytest_ml = np.vstack((ytest_ml, a))
+        ytest = a.reshape(1,(n-1)) 
+        
+    return ytest_ml
+    
+    
+def model_predict_lf2(testing_set, m, n, dt):
+    print("lf2")
+    custom_model = load_model('best_model.hd5',custom_objects={'coeff_determination': coeff_determination})
+    
+    ytest = [testing_set[0], testing_set[1]]
+    ytest = np.array(ytest)
+    ytest = ytest.reshape(1,2*(n-1))
+    
+    ytest_ml = [testing_set[0]]
+    ytest_ml = np.array(ytest_ml)
+    ytest_ml = np.vstack((ytest_ml, testing_set[1]))
+    
+    for i in range(2,testing_set.shape[0]):
+        slope_ml = custom_model.predict(ytest) # slope from LSTM/ ML model
+        a = ytest_ml[i-2] + 2.0*dt*slope_ml[0] # y1 at next time step
+        ytest_ml = np.vstack((ytest_ml, a))
+        e = a.reshape(1,n-1)
+        ytemp = ytest[0]
+        ytemp = ytemp.reshape(1,2*(n-1))
+        ee = np.concatenate((ytemp,e), axis = 1)
+        ee = ee[0,10:]
+        ytest = ee.reshape(1,2*(n-1))     
+    
+    return ytest_ml
+
+def model_predict_lf4(testing_set, m, n, dt):
+    print("lf4")
+    custom_model = load_model('best_model.hd5',custom_objects={'coeff_determination': coeff_determination})
+    
+    ytest = [testing_set[0], testing_set[1], testing_set[2], testing_set[3]]
+    ytest = np.array(ytest)
+    ytest = ytest.reshape(1,4*(n-1))
+    
+    ytest_ml = [testing_set[0]]
+    ytest_ml = np.array(ytest_ml)
+    ytest_ml = np.vstack((ytest_ml, testing_set[1]))
+    ytest_ml = np.vstack((ytest_ml, testing_set[2]))
+    ytest_ml = np.vstack((ytest_ml, testing_set[3]))
+    
+    for i in range(4,testing_set.shape[0]):
+        slope_ml = custom_model.predict(ytest) # slope from LSTM/ ML model
+        a = ytest_ml[i-2] + 2.0*dt*slope_ml[0] # y1 at next time step
+        ytest_ml = np.vstack((ytest_ml, a))
+        e = a.reshape(1,n-1)
+        ytemp = ytest[0]
+        ytemp = ytemp.reshape(1,4*(n-1))
+        ee = np.concatenate((ytemp,e), axis = 1)
+        ee = ee[0,10:]
+        ytest = ee.reshape(1,4*(n-1))  #np.vstack((ytest, ee)) # add [y1, y2, y3] at (n+1) to input test for next slope prediction
+    
+    return ytest_ml
+
+def model_predict_seq1(testing_set, m, n, dt):
+    print("seq1")
+    custom_model = load_model('best_model.hd5',custom_objects={'coeff_determination': coeff_determination})
+    
+    ytest = [testing_set[0]]
+    ytest = np.array(ytest)
+    
+    ytest_ml = [testing_set[0]]
+    ytest_ml = np.array(ytest_ml)
+    
+    for i in range(1,testing_set.shape[0]-1):
+        slope_ml = custom_model.predict(ytest) # slope from LSTM/ ML model
+        a = 1.0*slope_ml[0] # y1 at next time step
+        ytest_ml = np.vstack((ytest_ml, a))
+        ytest = a.reshape(1,1*(n-1)) 
+        
+    return ytest_ml
+    
--- SlopeNet/slopenet_main.py
+++ SlopeNet/slopenet_main.py
@@ -14,16 +14,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from create_data import * #create_training_data_bdf2
 from model_prediction import *
+from export_data import *
 
+#--------------------------------------------------------------------------------------------------------------#
 def coeff_determination(y_true, y_pred):
     SS_res =  K.sum(K.square( y_true-y_pred ))
     SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
     return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
+#--------------------------------------------------------------------------------------------------------------#
 dataset_train = pd.read_csv('./a.csv', sep=",",skiprows=0,header = None, nrows=1000)
 m,n=dataset_train.shape
 training_set = dataset_train.iloc[:,0:n].values
-
 dt = training_set[1,0] - training_set[0,0]
 training_set = training_set[:,1:n]
 
@@ -33,10 +35,11 @@ training_set = training_set[:,1:n]
 #training_set_scaled.shape
 #training_set = training_set_scaled
 
-legs = 2
-slopnet = "BDF"
+legs = 2 # No. of legs = 1,2,4
+slopnet = "LEAPFROG" # Choices: BDF, SEQ 
 xtrain, ytrain = create_training_data(training_set, m, n, dt, legs, slopnet)
 
+#--------------------------------------------------------------------------------------------------------------#
 from keras.models import Sequential, Model
 from keras.layers import Dense, dot, Input
 import keras.backend as K
@@ -44,6 +47,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from keras import optimizers
 
+#--------------------------------------------------------------------------------------------------------------#
 # create the LSTM model
 model = Sequential()
 
@@ -67,6 +71,7 @@ custom_model.compile(optimizer='adam', loss='mean_squared_error', metrics=[coeff
 history_callback = custom_model.fit(xtrain, ytrain, epochs=400, batch_size=100, verbose=1, validation_split= 0.2,
                                     callbacks=callbacks_list)
 
+#--------------------------------------------------------------------------------------------------------------#
 # training and validation loss. Plot loss
 loss_history = history_callback.history["loss"]
 val_loss_history = history_callback.history["val_loss"]
@@ -83,6 +88,7 @@ plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
+#--------------------------------------------------------------------------------------------------------------#
 #read data for testing
 dataset_test = pd.read_csv('./a.csv', sep=",",header = None, skiprows=1000)
 dataset_total = pd.concat((dataset_train,dataset_test),axis=0)
@@ -91,17 +97,10 @@ m,n=dataset_test.shape
 testing_set = dataset_test.iloc[:,0:n].values
 testing_set = testing_set[:,1:n]
 
-# predict the 
+#--------------------------------------------------------------------------------------------------------------#
+# predict results recursively using the model 
 ytest_ml = model_predict(testing_set, m, n, dt, legs, slopnet)
 
+# plot ML prediction and true data
+plot_results(ytest_ml, testing_set, m, n)
 
-for i in range(n-1):
-    plt.figure()    
-    plt.plot(ytest_ml[:,i], 'b-', label=r'$y_'+str(i+1)+'$'+'ML') 
-    plt.plot(testing_set[:,i], 'r-', label=r'$y_'+str(i+1)+'$') 
-    plt.ylabel('response ML')
-    plt.xlabel('time ML')
-    plt.legend(loc='best')
-    name = 'dns_bdf_twoleg_a='+str(i+1)+'.eps'
-    #plt.savefig(name, dpi = 600)
-    plt.show()
